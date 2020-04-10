c8956460092fdbe34b50431deba9b94155c32267
MaryVol <meryswhite@icloud.com>
2020-04-10 21:24:09 +0300

    towel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-
-// You should implement your task here.
+let matrix = [
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ],
+ ]
+ var conc = [].concat.apply([],matrix)
+ console.log(conc)
 
 module.exports = function towelSort (matrix) {
   return [];
